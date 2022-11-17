ff861d7afffe62d31414ab84bb435fc4118fcf95
mkilgi <mrt.kilgi@gmail.com>
2022-11-17 14:09:34 +0200

    *

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ const investment = document.querySelector('#investment');
 const interest = document.querySelector('#interest');
 const years = document.querySelector('#years');
 const submit = document.querySelector('#calculate');
+const canvas = document.querySelector('#graph')
 
 var total = 0;
 
@@ -16,6 +17,10 @@ function calculate() {
     // stats.appendChild(value);
     const value = document.querySelector('#value')
     value.innerHTML = total.toFixed(2)
-
 }
 
+var myChart = new Chart("myChart", {
+    type: "bar",
+    data: {},
+    options: {}
+  });
