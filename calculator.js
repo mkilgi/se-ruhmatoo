07af25e69bd404996ffb6c70535e90160435bbd1
mkilgi <mrt.kilgi@gmail.com>
2022-11-17 22:59:37 +0200

    added chart

--- calculator.js
+++ calculator.js
@@ -2,38 +2,50 @@ const investment = document.querySelector('#investment');
 const interest = document.querySelector('#interest');
 const years = document.querySelector('#years');
 const submit = document.querySelector('#calculate');
+const myChart1 = document.querySelector('#myChart');
+const graph = document.querySelector('.graph');
 
 var total = 0;
+var count = 0;
+var gains = 0
 
 function calculate() {
     total = investment.value * ((1 + interest.value / 100) ** years.value);
-    console.log(total);
-    const value = document.querySelector('#value')
-    value.innerHTML = total.toFixed(2) + '$'
+    gains = total / investment.value
 
-    var values = []
-    var labelYears = []
+    const value = document.querySelector('#value');
+    value.innerHTML = total.toFixed(2) + '$';
+
+    const g = document.querySelector('#gains');
+    g.innerHTML = gains.toFixed(2) + 'x'
+
+    var values = [];
+    var labelYears = [];
 
     for (let i = 1; i <= years.value; i++) {
         a = investment.value * ((1 + interest.value / 100) ** i);
-        labelYears.push(i)
-        values.push(a)
+        labelYears.push(i);
+        values.push(a);
     }
-
-    console.log(values)
-    console.log(labelYears)
 
     var myChart = new Chart("myChart", {
         type: "bar",
         data: {
             labels: labelYears,
             datasets: [{
-                backgroundColor:'white',
+                backgroundColor: 'green',
                 data: values
             }]
         },
-        options: {}
-      });
+        options: {
+            responsive: false,
+            legend: {
+                display: false
+            },
+            tooltips: {
+                enabled: false
+            }
+
+        }
+    });
 }
-
-
