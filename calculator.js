3379b288e1c6124bdc9bf57ba689f7329441c7be
mkilgi <mrt.kilgi@gmail.com>
2022-11-26 21:42:26 +0200

    *

--- calculator.js
+++ calculator.js
@@ -5,7 +5,14 @@ const submit = document.querySelector('#calculate');
 const myChart1 = document.querySelector('#myChart');
 const graph = document.querySelector('.graph');
 const stats = document.querySelector('.stats');
-const clearBtn = document.querySelector('#clear')
+const clearBtn = document.querySelector('#clear');
+const value = document.querySelector('#value');
+const g = document.querySelector('#gains');
+const tInt = document.querySelector('#totalInterest');
+const start = document.querySelector('#start');
+const butn = document.querySelector('#submit');
+const val = document.querySelector('.values');
+
 
 var total = 0;
 var gains = 0;
@@ -36,32 +43,22 @@ function calculate() {
     total = investment.value * ((1 + interest.value / 100) ** years.value);
     gains = total / investment.value
 
-    const value = document.querySelector('#value');
+    //Results
     value.innerHTML = total.toLocaleString('en-US', { maximumFractionDigits: 2 }) + '$';
-
-    const g = document.querySelector('#gains');
     g.innerHTML = gains.toLocaleString('en-US', { maximumFractionDigits: 2 }) + 'x';
-
-    const tInt = document.querySelector('#totalInterest');
     tInt.innerHTML = (total - investment.value).toLocaleString('en-US', { maximumFractionDigits: 2 }) + '$';
-
-    const start = document.querySelector('#start');
     start.innerHTML = (investment.value - 0).toLocaleString('en-US', { maximumFractionDigits: 2 }) + '$';
-
-    const butn = document.querySelector('#submit');
 
     labelYears = [];
     values = [];
 
     for (let i = 1; i <= years.value; i++) {
-        a = investment.value * ((1 + interest.value / 100) ** i);
+        a = (investment.value * ((1 + interest.value / 100) ** i)).toFixed(2);
         labelYears.push(i);
         values.push(a);
     }
 
-    myChart1.style.visibility = 'visible';
-    stats.style.visibility = 'visible';
-
+    //Colors based on investment returs
     if (interest.value < 0) {
         value.style.color = 'red';
         g.style.color = 'red';
@@ -69,6 +66,7 @@ function calculate() {
         start.style.color = 'red';
         myChart.data.datasets[0].backgroundColor = 'red';
         butn.style.backgroundColor = 'red';
+
     } else if (interest.value == 0) {
         value.style.color = 'white';
         g.style.color = 'white';
@@ -85,6 +83,8 @@ function calculate() {
         butn.style.backgroundColor = 'green';
     }
 
+    val.style.visibility = 'visible';
+
     myChart.data.datasets[0].data = values;
     myChart.data.labels = labelYears;
     myChart.update()
@@ -94,6 +94,8 @@ function clearInputs() {
     investment.value = ''
     interest.value = ''
     years.value = ''
+    val.style.visibility = 'hidden';
+    myChart.data.datasets[0].data = [];
+    myChart.data.labels = [];
+    myChart.update()
 }
-
-calculate();
